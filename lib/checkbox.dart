92fabebec0529fd889c732dc59cb1358ba9c2010
konhevalic <alan.patrick.konhevalic@gmail.com>
2021-10-28 21:14:43 -0300

    aprendendo a utilizar slider

--- lib/checkbox.dart
+++ lib/checkbox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/combustivel.dart';
 
 class EntradaCheckbox extends StatefulWidget {
   const EntradaCheckbox({ Key? key }) : super(key: key);
@@ -11,13 +12,21 @@ class _EntradaCheckboxState extends State<EntradaCheckbox> {
 
   bool? _comidaBrasileira = false;
   bool? _comidaMexicana = false;
+  bool _escolhaSwitch = false;
 
-  String? _escolhaUsuario;
+  double _valor = 5;
+  String label = "Valor selecionado";
+
+  List<String> _minhaLista = [];
+
+  Map<String, dynamic> _meuMap = {};
+
+  String _escolhaUsuario = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Checkbox e RadioButtons"),),
+      appBar: AppBar(title: const Text("Checkbox e RadioButtons"),),
       body: Container(
         child: Column(
           children: [
@@ -35,12 +44,12 @@ class _EntradaCheckboxState extends State<EntradaCheckbox> {
               }
             ),
             CheckboxListTile(
-              title: const Text("Comida brasileira"),
+              title: const Text("Comida mexicana"),
               subtitle: const Text("A melhor comida"),
               //selected: true,
               secondary: const Icon(Icons.add_box),
               activeColor: Colors.red,
-              value: _comidaMexicana, 
+              value: _comidaMexicana,
               onChanged: (bool? valor) {
                 setState(() {
                   _comidaMexicana = valor;
@@ -55,7 +64,7 @@ class _EntradaCheckboxState extends State<EntradaCheckbox> {
                   groupValue: _escolhaUsuario,
                   onChanged: (String? escolha) {
                     setState(() {
-                      _escolhaUsuario = escolha;
+                      _escolhaUsuario = escolha!;
                     });
                   },
                 ),
@@ -65,17 +74,41 @@ class _EntradaCheckboxState extends State<EntradaCheckbox> {
                   groupValue: _escolhaUsuario,
                   onChanged: (String? escolha) {
                     setState(() {
-                      _escolhaUsuario = escolha;
+                      _escolhaUsuario = escolha!;
                     });
                   },
                 ),
+                SwitchListTile(
+                  title: const Text("Receber notificações"),
+                  value: _escolhaSwitch, 
+                  onChanged: (bool valor) {
+                    setState(() {
+                      _escolhaSwitch = valor;
+                    });
+                  }
+                ),
+                Slider(
+                  min: 0,
+                  max: 10,
+                  divisions: 10,
+                  label: label,
+                  value: _valor, 
+                  onChanged: (double novoValor) {
+                    setState(() {
+                      _valor = novoValor;
+                      label = "Valor selecionado: " + novoValor.toString();
+                    });
+                    print("Valor selecionado" + novoValor.toString());
+                  },
+                )
               ],
             ),
             ElevatedButton(
               onPressed: () {
                 print("comida brasileira:" + _comidaBrasileira.toString());
                 print("comida mexicana:" + _comidaMexicana.toString());
-                print("a pessoa que escolheu é do sexo: " + _escolhaUsuario!);
+                print("a pessoa que escolheu é do sexo: " + _escolhaUsuario);
+                print("receber notificaçoes: " + _escolhaSwitch.toString());
               }, 
               child: const Text(
                 "Salvar",
@@ -83,7 +116,8 @@ class _EntradaCheckboxState extends State<EntradaCheckbox> {
                   fontSize: 20
                 ),
               )
-            )
+            ),
+
           ],
         ),
       ),
